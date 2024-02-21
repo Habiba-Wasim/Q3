145897a85755138650c748d8006747983daf7d4c
Habiba-Wasim <160347457+Habiba-Wasim@users.noreply.github.com>
2024-02-21 16:06:07 +0500

    Step2 Of Typescript
    Step2 Of Typescript

---
+++ Q3.js
@@ -0,0 +1,6 @@
+"use strict";
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let firstName = "Habiba";
+console.log(firstName.toLowerCase());
+console.log(firstName.toUpperCase());
+console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1));
